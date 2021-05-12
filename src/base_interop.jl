0d234c8133ed926af46fd66258861709f3b52c78
Chris Foster <chris42f@gmail.com>
2021-05-12 14:33:25 +1000

    Base interop: tests + stream redirection
    * Tests for open, mktemp, mktempdir, lock
    * Implement redirect_{stdin,stderr,stdout}

--- src/base_interop.jl
+++ src/base_interop.jl
@@ -21,10 +21,34 @@ end
 function Base.mktempdir(ctx::AbstractContext, parent=tempdir(); prefix="jl_")
     path = mktempdir(parent, cleanup=false)
     @defer ctx rm(path, recursive=true, force=true)
-    (path, io)
+    path
 end
 
 function Base.lock(ctx::AbstractContext, lk::Base.AbstractLock)
     lock(lk)
     @defer ctx unlock(lk)
 end
+
+
+#-------------------------------------------------------------------------------
+function Base.redirect_stdout(ctx::AbstractContext, stream)
+    prev_stream = stdout
+    x = redirect_stdout(stream)
+    @defer ctx redirect_stdout(prev_stream)
+    x
+end
+
+function Base.redirect_stderr(ctx::AbstractContext, stream)
+    prev_stream = stderr
+    x = redirect_stderr(stream)
+    @defer ctx redirect_stderr(prev_stream)
+    x
+end
+
+function Base.redirect_stdin(ctx::AbstractContext, stream)
+    prev_stream = stdin
+    x = redirect_stdin(stream)
+    @defer ctx redirect_stdin(prev_stream)
+    x
+end
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,15 @@
 using Contexts
 using Test
 
-function foo(ctx::AbstractContext, do_cleanup)
+function foo(ctx::AbstractContext, x)
     # Use of @defer with explicit context
-    @defer ctx do_cleanup()
+    @defer ctx push!(x, :A)
 end
 
 @testset "Cleanup ordering" begin
     cleanups = []
     @context begin
-        @! foo(()->push!(cleanups, :A))
+        @! foo(cleanups)
         @defer push!(cleanups, :B)
         @test cleanups == []
     end
@@ -19,7 +19,6 @@ end
 @testset "Exceptions during cleanup" begin
     try
         @context begin
-            @! foo(()->push!(cleanups, :A))
             @defer error("A")
             @defer error("B")
         end
@@ -30,5 +29,94 @@ end
     end
 end
 
-@testset "Contexts Base interop" begin
+@testset "Base interop" begin
+    @testset "open() and mktemp()" begin
+        path = ""
+        io1 = nothing
+        io2 = nothing
+        io3 = nothing
+        io4 = nothing
+        @context begin
+            (path,io1) = @! mktemp()
+            msg = "hi from Contexts.jl"
+            write(io1, msg)
+            flush(io1)
+            # Test various forms of open()
+            io2 = @! open(path)
+            @test read(io2, String) == msg
+            io3 = @! open(path, "r")
+            @test read(io3, String) == msg
+            io4 = @! open(path, read=true)
+            @test read(io4, String) == msg
+        end
+        @test !isempty(path) && !ispath(path)
+        @test !isopen(io1)
+        @test !isopen(io2)
+        @test !isopen(io3)
+        @test !isopen(io4)
+    end
+
+    @testset "mktemp() and mktempdir()" begin
+        parent_dir = nothing
+        dir2 = nothing
+        filepath = nothing
+        @context begin
+            parent_dir = @! mktempdir()
+            # forms with parent dirs
+            dir2 = @! mktempdir(parent_dir)
+            (filepath,_) = @! mktemp(parent_dir)
+            # All resources are created
+            @test isdir(parent_dir)
+            @test isdir(dir2)
+            @test isfile(filepath)
+            @test parent_dir == dirname(dir2)
+            @test parent_dir == dirname(filepath)
+        end
+        # All resources are cleaned up
+        @test !ispath(parent_dir)
+        @test !ispath(dir2)
+        @test !ispath(filepath)
+    end
+
+    @testset "lock()" begin
+        lk = ReentrantLock()
+        @context begin
+            @! lock(lk)
+            @test islocked(lk)
+        end
+        @test !islocked(lk)
+    end
+
+    @testset "redirect_stdout / stderr / stdin" begin
+        orig_stdin = stdin
+        orig_stdout = stdout
+        orig_stderr = stderr
+        @context begin
+            (_,io) = @! mktemp()
+            @! redirect_stdout(io)
+            println("hi")
+            flush(io)
+            seek(io, 0)
+            @test readline(io) == "hi"
+        end
+        @context begin
+            (_,io) = @! mktemp()
+            @! redirect_stderr(io)
+            println(stderr, "hi")
+            flush(io)
+            seek(io, 0)
+            @test readline(io) == "hi"
+        end
+        @context begin
+            (_,io) = @! mktemp()
+            println(io, "hi")
+            flush(io)
+            seek(io,0)
+            @! redirect_stdin(io)
+            @test readline() == "hi"
+        end
+        @test orig_stdin == stdin
+        @test orig_stdout == stdout
+        @test orig_stderr == stderr
+    end
 end
